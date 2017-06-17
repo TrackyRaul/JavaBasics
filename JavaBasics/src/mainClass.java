d3e98d2033686365e6b678a73358b4c9194434ac
TrackyRaul <raul.farkas19@gmail.com>
2017-06-17 12:29:56 +0200

    1

--- JavaBasics/src/mainClass.java
+++ JavaBasics/src/mainClass.java
@@ -13,7 +13,7 @@ public class mainClass {
 		Object numero = sc.next();
 		
 		if(((String) numero).matches("[a-zA-Z]+")){
-			System.out.println("Inserisci un numero non una lettera!!");
+			System.out.println("Inserisci un numero non una lettera!1!");
 			
 		}if(((String) numero).matches("[0-9]+")){
 			System.out.println("Bravo hai inserito un numero");
