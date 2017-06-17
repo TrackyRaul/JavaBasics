4582470179f710f53c1e5913d88a17286aef18bc
TrackyRaul <raul.farkas19@gmail.com>
2017-06-17 11:49:10 +0200

    Basics: variables,systemout and function.

---
+++ JavaBasics/src/mainClass.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class mainClass {
+	
+
+	public static void main(String[] args) {
+		Scanner text = new Scanner(System.in);
+		
+		int Ciao = 34;
+		
+		
+		
+		String lol = "sdafd";
+		double dlfkjsk = 13.4;
+		System.out.print("Aggiungi: " );
+		String user = text.next();
+		System.out.println(Ciao+" "+lol+ " "+ user);
+		ciccio("sdafds");
+
+	}
+	
+	
+	public static void ciccio(String cane){
+		System.out.println(cane);
+	}
+
+}
