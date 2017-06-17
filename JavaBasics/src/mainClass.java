4f33afae1c10c669ca189726e849595103979453
TrackyRaul <raul.farkas19@gmail.com>
2017-06-17 12:24:52 +0200

    Prog Pronto finalmente

--- JavaBasics/src/mainClass.java
+++ JavaBasics/src/mainClass.java
@@ -4,9 +4,25 @@ public class mainClass {
 	
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		
-		Object numero;
+		System.out.print("Inserisci un numero: ");
+		
+		
+		Object numero = sc.next();
+		
+		if(((String) numero).matches("[a-zA-Z]+")){
+			System.out.println("Inserisci un numero: ");
+			
+		}if(((String) numero).matches("[0-9]+")){
+			System.out.println("Bravo hai inserito un numero");
+		}
+		
+		
+		
+		
+		
 		
 		
 		
