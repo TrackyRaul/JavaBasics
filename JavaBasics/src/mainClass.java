04c637add0e1e5deb81689b43acd2a2ed65ce1bc
TrackyRaul <raul.farkas19@gmail.com>
2017-06-17 12:39:40 +0200

    LOOP

--- JavaBasics/src/mainClass.java
+++ JavaBasics/src/mainClass.java
@@ -5,20 +5,24 @@ public class mainClass {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int x = 0;
+		while(x == 0){
 		
-		
-		System.out.print("Inserisci un numero: ");
-		
-		
-		Object numero = sc.next();
-		
-		if(((String) numero).matches("[a-zA-Z]+")){
-			System.out.println("Inserisci un numero non una lettera!1!");
+			System.out.print("Inserisci un numero: ");
 			
-		}if(((String) numero).matches("[0-9]+")){
-			System.out.println("Bravo hai inserito un numero");
-		}
-		
+			
+			Object numero = sc.next();
+			
+			if(((String) numero).matches("[a-zA-Z]+")){
+				System.out.println("Inserisci un numero non una lettera!1!");
+				x = 0;
+				
+			}if(((String) numero).matches("[0-9]+")){
+				System.out.println("Bravo hai inserito un numero");
+				x = 1;
+			}
+			
+			}
 		
 		
 		
